77dc541d10904c09c8953e97fa61be59ac520bb7
Zauzanov <zauzanov@gmail.com>
2025-12-31 13:00:15 +0300

    added comments

--- README.md
+++ README.md
@@ -1,20 +1,52 @@
 # cord-rover
 `cord-rover` is an e-mail(POP3;IMAP;SMTP) packets analyzer based on Scapy.
-## Run:
+## 1. Run:
 ```bash
 sudo python coro.py
 ```
-## We will use telnet to simulate a manual SMTP login to Metasploitable2:
+## 2. We will use telnet to simulate a manual SMTP login to Metasploitable2:
 ```bash
 telnet [META2_IP] 25
+# Example: telnet 192.168.204.129 25
 ```
-## Once connected, type the following (the SMTP server will respond to each line):
+## 3. Once connected, type the following (the SMTP server will respond to each line):
 ```bash
-HELO test.com
+HELO test.com                      # To initiate communication with a SMTP server. 
 AUTH LOGIN
 USER admin
 PASS password123
 ```
+## 4. Like this:  
+```bash
+telnet 192.168.204.129 25
+Trying 192.168.204.129...
+Connected to 192.168.204.129.
+220 metasploitable.localdomain ESMTP Postfix (Ubuntu)
+HELO test.com
+250 metasploitable.localdomain
+AUTH LOGIN
+USER my_secret
+PASS my_pass
+```
+## 5. The script's output:
+```bash
+sudo python coro.py
+[*] Sniffing... Try typing in your Telnet window now.
+[DEBUG] Received data: auth login
+[DEBUG] Received data: 503 5.5.1 error: authentication not enabled
+[DEBUG] Received data: user my_secret
+
+[!] ALERT: Credentials found!
+[*] Destination: 192.168.204.129
+[*] Payload: user my_secret
+[DEBUG] Received data: 502 5.5.2 error: command not recognized
+[DEBUG] Received data: pass my_pass
+
+[!] ALERT: Credentials found!
+[*] Destination: 192.168.204.129
+[*] Payload: pass my_pass
+```
+
 <br>
 <br>
 **Note**: the BPF filter can be edited to monitor other traffic: for example, to monitor FTP connections and credentials, change it to `tcp port 21` in `coro.py` file. 
--- coro.py
+++ coro.py
@@ -1,21 +1,29 @@
-from scapy.all import sniff, TCP, IP
+from scapy.all import sniff, TCP, IP                                                # sniff interacts with the NIC to capture raw data.
 
-def packet_callback(packet):
-    if packet.haslayer(TCP) and packet.haslayer(IP):
-        raw_payload = packet[TCP].payload
-        payload_str = bytes(raw_payload).decode('utf-8', errors='ignore').lower()
+def packet_callback(packet):                                                        # This func is executed every time a packet passes the filter.  
+    if packet.haslayer(TCP) and packet.haslayer(IP):                                # Ensures the script doesn't crash: if a packet is an ARP or ICMP packet, it will not have a TCP layer.  
+        raw_payload = packet[TCP].payload                                           # Extractes the data, digging into the 'data' portin of the TCP segment.
+        payload_str = bytes(raw_payload).decode('utf-8', 
+                                                errors='ignore').lower()            # Converts bytes into strings safely. Also I use `errors="ignore"` to prevent crashes on non-text data.
         
-        if len(payload_str) > 0:
+
+        # DEBUG mode prints every packet's first few chars 
+        # to prove we are hearing the traffic
+        if len(payload_str) > 0:                                                    # Filters zero data packets(ACK or SYN handshakes), ignoring those empty management packets to save processing time. 
              print(f"[DEBUG] Received data: {payload_str.strip()}")
 
-        if 'user' in payload_str or 'pass' in payload_str:
+        # Signagute-based detection
+        if 'user' in payload_str or 'pass' in payload_str:                          # It looks for the specific patterns used by SMPT, POP3 and IMAP whe a user tries to auth. 
             print(f"\n[!] ALERT: Credentials found!")
-            print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] Destination: {packet[IP].dst}")                             # Extracts the metadata, pulling the dest IP-address from the L3(IP) header, telling us which server the user is sending creds to.
             print(f"[*] Payload: {payload_str.strip()}")
 
 def main(): 
     print("[*] Starting sniffer on email ports...")
-    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', 
+          prn=packet_callback, store=0)                                             # Looks for SMPT/POP3/IMAP ports only; 
+                                                                                    # Using prn, I link the sniffer to my func;  
+                                                                                    # store=0 ensures we don't run out of RAM during long captures, bc by default Scapy keeps every packet in RAM. 
 
 if __name__ == '__main__':
     main()
